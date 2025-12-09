356a66f8c8fe671764572af65af1458374f127b2
blendzeqiri <blendzeqiri8@gmail.com>
2025-12-09 19:50:52 +0100

    54

--- example1.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-
-html_content = "<html><body><p>Hello, Beautiful Soup!</p></body></html>"
-
-soup = BeautifulSoup(html_content, 'html.parser')
-
-paragraph_text = soup.find('p').text
-
-print(paragraph_text)
