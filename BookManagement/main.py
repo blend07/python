d4a83e69e2242923277362a0a22c3e6184df072b
blend <blendzeqiri8@gmail.com>
2025-12-11 20:31:17 +0100

    55

---
+++ BookManagement/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from routers import authors, books, api_key
+from database import create_database
+
+app = FastAPI(
+    title="Book Managemenet System"
+    description="An API for Managing books, authors, genres etc"
+    version="1.0.0",
+)
+app.include_router(authors.router, prefix="/api/authors", tags=["Authors"])
+app.include_router(books.router, prefix="/api/books", tags=["Books"])
+app.include_router(api_key.router, prefix="/api/validate_keys")
+
+@app.on_event("startup")
+def starup():
+    create_database()
